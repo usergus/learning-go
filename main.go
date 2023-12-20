2aa16f73b11326358f7f8286eb423ce9f5815cee
usergus <usergus@usergus.local>
2023-12-20 21:01:42 +0100

    first try golang

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+// comment
+import "fmt"
+
+func main() {
+	var (
+		x int
+		y string
+		z bool
+	)
+
+	x = 28
+	y = "Toto"
+
+	fmt.Printf("Mon age est : %v\nMon nom est : %v\nMon nombre est : %v", x, y, z)
+
+}
